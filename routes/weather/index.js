403895f30cd0db56640c9c566370ca7d3621f9d4
Peter Peerdeman <peter@raspberrypi.(none)>
2015-09-21 22:05:02 +0200

    make requests to relative location

--- routes/weather/index.js
+++ routes/weather/index.js
@@ -2,12 +2,9 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-router.get('/temperatures', function (req, res) {
-    var url = 'http://127.0.0.1:1234/temperatures';
-    if (req.query.limit) {
-        url += '?limit='+req.query.limit;
-    }
-    request.get(url).pipe(res);
+router.all('/*', function (req, res) {
+    var url = 'http://127.0.0.1:1234' + req.url;
+    req.pipe(request(url)).pipe(res);
 })
 
 module.exports = router;
