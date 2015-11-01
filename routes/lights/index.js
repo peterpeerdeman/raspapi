c2034ba62a23a40afd6ffa809789f94760e0be2e
Peter Peerdeman <peter@lifely.nl>
2015-11-01 12:35:29 +0100

    feat(lights): added more controls to hue lights

--- routes/lights/index.js
+++ routes/lights/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var hue = require('node-hue-api');
 var q = require('q');
 var HueApi = hue.HueApi;
+var lightState = hue.lightState;
 
 var hostname = '192.168.117.28';
 var username = '';
@@ -33,7 +34,7 @@ router.get('/lights/details', function(req, res) {
         .then(function(result) {
             var data = [];
             res.send(result);
-        }, 
+        },
         function() {
             res.sendStatus(500);
         });
@@ -44,6 +45,44 @@ router.get('/lights/:id', function(req, res) {
     api.lightStatus(req.params.id)
         .then(function(result) {
             res.send(result);
+        })
+        .done();
+});
+
+router.get('/lights/:id/on', function(req, res) {
+    api.setLightState(req.params.id, {'on': true})
+        .then(function(result) {
+            res.send(result);
+        })
+        .fail(function(error) {
+            res.send(error);
+        })
+        .done();
+});
+
+router.get('/lights/:id/off', function(req, res) {
+    api.setLightState(req.params.id, {'on': false})
+        .then(function(result) {
+            res.send(result);
+        })
+        .fail(function(error) {
+            res.send(error);
+        })
+        .done();
+});
+
+router.get('/lights/:id/random', function(req, res) {
+    api.setLightState(req.params.id, {
+        'xy': [
+            Math.random(),
+            Math.random()
+        ],
+    })
+        .then(function(result) {
+            res.send(result);
+        })
+        .fail(function(error) {
+            res.send(error);
         })
         .done();
 });
@@ -76,6 +115,44 @@ router.get('/on', function(req, res) {
         .done();
 });
 
+router.get('/brightness/inc', function(req, res) {
+    api.lights(function(err, result) {
+        if (err) throw err;
+        var promises = [];
+        result.lights.forEach(function(light) {
+            var state = lightState.create(light);
+            promises.push(api.setLightState(light.id, state.bri_inc(50)));
+        });
+        q.all(promises)
+        .then(function(result) {
+            var data = [];
+            res.send(result);
+        },
+        function() {
+            res.sendStatus(500);
+        });
+    });
+});
+
+router.get('/brightness/dec', function(req, res) {
+    api.lights(function(err, result) {
+        if (err) throw err;
+        var promises = [];
+        result.lights.forEach(function(light) {
+            var state = lightState.create(light);
+            promises.push(api.setLightState(light.id, state.bri_inc(-50)));
+        });
+        q.all(promises)
+        .then(function(result) {
+            var data = [];
+            res.send(result);
+        },
+        function() {
+            res.sendStatus(500);
+        });
+    });
+});
+
 router.get('/off', function(req, res) {
     api.setGroupLightState(0, {'on': false}) // provide a value of false to turn off
         .then(function(result) {
@@ -85,6 +162,45 @@ router.get('/off', function(req, res) {
             res.send(error);
         })
         .done();
+});
+
+router.get('/randomcolor', function(req, res) {
+    api.setGroupLightState(0, {
+        'xy': [
+            Math.random(),
+            Math.random()
+        ]
+    })
+        .then(function(result) {
+            res.send(result);
+        })
+        .fail(function(error) {
+            res.send(error);
+        })
+        .done();
+});
+
+router.get('/randomcolors', function(req, res) {
+    api.lights(function(err, result) {
+        if (err) throw err;
+        var promises = [];
+        result.lights.forEach(function(light) {
+            promises.push(api.setLightState(light.id, {
+                'xy': [
+                    Math.random(),
+                    Math.random()
+                ]
+            }));
+        });
+        q.all(promises)
+        .then(function(result) {
+            var data = [];
+            res.send(result);
+        },
+        function() {
+            res.sendStatus(500);
+        });
+    });
 });
 
 module.exports = router;
