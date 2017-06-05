0a9d48f0b2360058a85ca995aa4a23e871b0aade
Peter Peerdeman <peter@lifely.nl>
2017-06-05 19:31:02 +0200

    feat(mediacenter): add updownleftright select controls

--- routes/mediacenter/index.js
+++ routes/mediacenter/index.js
@@ -4,6 +4,61 @@ var router = express.Router();
 var _ = require('lodash');
 var request = require('request');
 
+router.get('/select', function(req, res) {
+    request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Input.Select"}', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+router.get('/right', function(req, res) {
+    request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Input.Right"}', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+router.get('/left', function(req, res) {
+    request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Input.Left"}', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+router.get('/down', function(req, res) {
+    request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Input.Down"}', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+router.get('/up', function(req, res) {
+    request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Input.Up"}', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
 router.get('/quit', function(req, res) {
     request('http://' + process.env.XBMC_HOST + ':' + process.env.XBMC_PORT + '/jsonrpc?request={"jsonrpc": "2.0", "id": 1, "method": "Application.Quit"}', function(err, data) {
         if (err) {
