5f2e511c858758bcd3c9ff8943ab35e47e58ffe8
Peter Peerdeman <peter@lifely.nl>
2019-12-28 20:17:14 +0100

    feat(top): changed top functionality to retrieve stats from influx

--- routes/top/index.js
+++ routes/top/index.js
@@ -1,72 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var parser = require('./parser');
-var proc   = require('child_process');
+const Influx = require('influxdb-nodejs');
+const influxClient = new Influx('http://127.0.0.1:8086/raspimetrics');
 
-router.get('/', function(req, res) {
-    var command = '';
-    var limit = Math.min(req.query.limit, 10) || 10;
+router.get('/cpu', function(req, res) {
+    let query = influxClient
+    .query('cpu')
+    .addFunction('mean', 'usage_user')
+    .where('cpu','cpu-total','=')
+    .addGroup('time(10m)');
+    query.start = '-1h';
+    query.end = 'now()';
+    query.then((result) => {
+        res.send(result);
+    })
+    .catch(console.error);
+});
 
-    switch (require('os').platform()) {
-        case 'darwin':
-            //2 samples have to be taken, first one doesnt have CPU values
-            command = 'top -u -l 2';
+router.get('/disk', function(req, res) {
+    let query = influxClient
+    .query('disk')
+    .addFunction('mean', 'used')
+    .addGroup('time(10m)');
+    query.start = '-1h';
+    query.end = 'now()';
+    query.then((result) => {
+        res.send(result);
+    })
+    .catch(console.error);
+});
 
-            proc.exec(command, function(err, stdout) {
-
-                if (err) {
-                    return done(err);
-                }
-
-                var bothSamples = stdout.toString();
-                var samplesArray = bothSamples.split(/Processes:/);
-                var secondSample = 'Processes:' + samplesArray[2];
-
-                parser.parse(secondSample, 11, function(err, result) {
-
-                    if (err) {
-                        throw err;
-                    }
-
-                    var topJson = JSON.parse(result);
-
-                    var sorted = topJson.sort(function(a, b) {
-                        return parseFloat(b['%CPU']) - parseFloat(a['%CPU']);
-                    });
-
-                    res.send(sorted.slice(0, limit));
-                });
-            });
-            break;
-
-        case 'linux':
-            command = 'top -bn 1';
-            proc.exec(command, function(err, stdout) {
-                if (err) {
-                    throw err;
-                }
-                parser.parse(stdout.toString(), 6, function(err, result) {
-                    if (err) {
-                        throw err;
-                    }
-
-                    var topJson = JSON.parse(result);
-
-                    var sorted = topJson.sort(function(a, b) {
-                        return parseFloat(b['%CPU']) - parseFloat(a['%CPU']);
-                    });
-
-                    res.send(sorted.slice(0, limit));
-                });
-            });
-
-            break;
-
-        default:
-            throw new Error('does not work on other platforms than darwin and linux');
-    }
-
+router.get('/net', function(req, res) {
+    let query = influxClient
+    .query('net')
+    .addFunction('non_negative_derivative(mean(bytes_recv),1s)*8', {
+        alias:'in' 
+    })
+    .addFunction('non_negative_derivative(mean(bytes_sent),1s)*8', {
+        alias:'out' 
+    })
+    .addGroup('time(10m)');
+    query.start = '-1h';
+    query.end = 'now()';
+    query.then((result) => {
+        res.send(result);
+    })
+    .catch(console.error);
 });
 
 module.exports = router;
