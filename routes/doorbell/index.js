af07d6c824cf27e20c6bf1ea88f407811f1ce3a3
Peter Peerdeman <peter@lifely.nl>
2020-03-07 23:23:04 +0100

    feat(doorbell): add doorbell webhook and retrieval

---
+++ routes/doorbell/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const Influx = require('influxdb-nodejs');
+const influxClient = new Influx('http://127.0.0.1:8086/doorbell');
+
+router.get('/rings', function(req, res) {
+    let query = influxClient
+    .query('ring');
+    query.start = '-7d';
+    query.end = 'now()';
+    query.then((result) => {
+        res.send(result);
+    })
+    .catch(console.error);
+});
+
+router.post('/webhook', function(req, res) {
+    /* webhook has following format
+{
+  "event": "{{{PARTICLE_EVENT_NAME}}}",
+  "data": "{{{PARTICLE_EVENT_VALUE}}}",
+  "coreid": "{{{PARTICLE_DEVICE_ID}}}",
+  "published_at": "{{{PARTICLE_PUBLISHED_AT}}}"
+}
+    */
+    console.log(req.body);
+    influxClient.write('ring').field({
+        event: req.body.event,
+        coreid: req.body.coreid,
+        data: req.body.data,
+        published_at: req.body.published_at
+    }).then(() => {
+        console.debug(`${Date.now()} doorbell: influx write point success`);
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.debug(`${Date.now()} doorbell: retrieve temperature failed ${error}`);
+        res.sendStatus(500);
+    });
+
+});
+
+module.exports = router;
