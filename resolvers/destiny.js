c2e127d9ff0dabcd5f70a56853f1be9d16847117
Peter Peerdeman <peter@lifely.nl>
2020-06-08 01:24:30 +0200

    feat(destiny): create destiny resolver

---
+++ resolvers/destiny.js
@@ -0,0 +1,23 @@
+const Influx = require('influxdb-nodejs');
+const influxClient = new Influx(process.env.INFLUXDB_HOST + 'destiny');
+
+module.exports.default = (parent, args, context, info) => {
+    let query = influxClient
+    .query('clanmemberstatus')
+    .addFunction('last', 'isOnline')
+    .addGroup('displayName');
+    query.start = '-30m';
+    query.end = 'now()';
+    return query.then((result) => {
+        const memberSeries = result.results[0].series;
+        const presence = memberSeries.map((member) => {
+            return {
+                name: member.tags.displayName,
+                timestamp: member.values[0][0],
+                isOnline: member.values[0][1]
+            };
+        });
+        return presence;
+    })
+    .catch(console.error);
+};
