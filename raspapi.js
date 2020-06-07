e6dc4d12133b33780ffe569abd7bd71930bd8222
Peter Peerdeman <peter@lifely.nl>
2020-06-08 01:24:53 +0200

    feat(graphql): complete graphql api with destiny type and resolver

--- raspapi.js
+++ raspapi.js
@@ -6,44 +6,29 @@ const fs = require( 'fs' );
 const auth = require('basic-auth');
 const bodyParser = require('body-parser');
 
+const destiny_resolver = require('./resolvers/destiny.js').default;
+
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
-        books: [Book]
         hello: String
+        destinyPresence: [DestinyPresence]
+    }
+
+    type DestinyPresence {
+        name: String
+        isOnline: Boolean
+        timestamp: String
     }
 `;
 
-const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
-];
-
 const resolvers = {
     Query: {
-            hello: () => 'world',
-            books: () => books,
+        hello: () => 'world',
+        destinyPresence: destiny_resolver
     },
 };
-
 
 const server = new ApolloServer({
     typeDefs,
@@ -53,6 +38,21 @@ const server = new ApolloServer({
             myProperty: true
         };
     },
+    playground: {
+        settings: {
+            'editor.theme': 'light',
+        },
+        tabs: [
+            {
+                endpoint: 'http://localhost:2999/graphql',
+                query: 'query {\n  hello\n}',
+                name: 'Raspapi',
+                headers: {
+                    'Authorization': `Basic ${new Buffer.from(process.env.API_USER + ":" + process.env.API_PASS, 'utf-8').toString("base64")}`
+                }
+            },
+        ],
+    },
 });
 
 //parsing
@@ -61,18 +61,18 @@ app.use(bodyParser.json({
 }));
 
 //authorization
-// app.use(function(req, res, next) {
-//     var credentials = auth(req);
-//     if (!credentials || credentials.name !== process.env.API_USER || credentials.pass !== process.env.API_PASS) {
-//         res.set({
-//             'WWW-Authenticate': 'Basic realm="simple-admin"'
-//         });
-//         res.sendStatus(401);
-//     } else {
-//         next();
-//     }
-// });
-//
+app.use(function(req, res, next) {
+    var credentials = auth(req);
+    if (!credentials || credentials.name !== process.env.API_USER || credentials.pass !== process.env.API_PASS) {
+        res.set({
+            'WWW-Authenticate': 'Basic realm="simple-admin"'
+        });
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+});
+
 //pretty jsonresponses
 app.use(function(req, res, next) {
     res.sendStatusJson = function (status) {
