924a4ec2ae6ce684a5dd3512040216c9ba4439c3
Peter Peerdeman <peter@lifely.nl>
2020-06-28 15:43:34 +0200

    feat(cluster): omit device id in config, use first and only device when available

--- modules/cluster.js
+++ modules/cluster.js
@@ -1,51 +1,34 @@
 const Unifi = require('node-unifi-poecontrol');
 const unifi = new Unifi(process.env.CLUSTER_UNIFI_HOST);
 
-const DEVICE_ID = process.env.CLUSTER_UNIFI_DEVICE_ID;
-
 module.exports.getDevices = function() {
     return unifi.login(process.env.CLUSTER_UNIFI_USERNAME, process.env.CLUSTER_UNIFI_PASSWORD).then(loginresponse => {
         return unifi.getDevices();
     });
 };
 
-module.exports.getDevice = function() {
+module.exports.getDevice = function(device_id) {
     return unifi.login(process.env.CLUSTER_UNIFI_USERNAME, process.env.CLUSTER_UNIFI_PASSWORD).then(loginresponse => {
         return unifi.getDevices().then(response => {
             const device = response.data.find(element => {
-                return element.device_id === DEVICE_ID;
+                return element.device_id === device_id;
             });
             return device;
         });
     });
 };
 
-module.exports.getPortTable = function() {
-    return unifi.login(process.env.CLUSTER_UNIFI_USERNAME, process.env.CLUSTER_UNIFI_PASSWORD).then(loginresponse => {
-        return unifi.getDevices().then(response => {
-            const device = response.data.find(element => {
-                return element.device_id === DEVICE_ID;
-            });
-            return device.port_table;
-        });
+module.exports.getPortTable = function(device_id) {
+    return module.exports.getDevice(device_id).then(device => {
+        console.log(device);
+        return device.port_table;
     });
 };
 
-module.exports.getPortTable = function() {
-    return unifi.login(process.env.CLUSTER_UNIFI_USERNAME, process.env.CLUSTER_UNIFI_PASSWORD).then(loginresponse => {
-        return unifi.getDevices().then(response => {
-            const device = response.data.find(element => {
-                return element.device_id === DEVICE_ID;
-            });
-            return device.port_table;
-        });
-    });
-};
-
-module.exports.setPoePorts = function(portConfig) {
+module.exports.setPoePorts = function(device_id, portConfig) {
     return unifi.login(process.env.CLUSTER_UNIFI_USERNAME, process.env.CLUSTER_UNIFI_PASSWORD).then(loginresponse => {
         // set first and second poe port to be enabled, third one to be disabled
-        return unifi.setPoePorts(DEVICE_ID, portConfig).then(response => {
+        return unifi.setPoePorts(device_id, portConfig).then(response => {
             return response;
         });
     });
--- resolvers/cluster.js
+++ resolvers/cluster.js
@@ -1,30 +1,49 @@
 const cluster = require('../modules/cluster');
 
 module.exports.portTable = (parent, args, context, info) => {
-    return cluster.getPortTable();
+    return cluster.getDevices().then(devices => {
+        if(devices.data.length == 0) {
+            throw new Error('no devices detected to get porttables from');
+        };
+        if(devices.data.length > 1) {
+            throw new Error('more than 1 device detected, dont know which one to address');
+        };
+        return cluster.getPortTable(devices.data[0].device_id);
+    });
 };
 
 module.exports.cluster_scale = (parent, args, context, info) => {
-    let promise;
-    switch(args.instances) {
-        case 0:
-            promise = cluster.setPoePorts([false, false, false]);
-        break;
-        case 1:
-            promise = cluster.setPoePorts([true, false, false]);
-        break;
-        case 2:
-            promise = cluster.setPoePorts([true, true, false]);
-        break;
-        case 3:
-            promise = cluster.setPoePorts([true, true, true]);
-        break;
-        default:
-            throw new Error('this number of instances is not supported');
-        break;
-    }
+    return cluster.getDevices().then(devices => {
+        if(devices.data.length == 0) {
+            throw new Error('no devices detected to get porttables from');
+        };
+        if(devices.data.length > 1) {
+            throw new Error('more than 1 device detected, dont know which one to address');
+        };
 
-    return promise.then(response => {
-        return [];
+        const device_id = devices.data[0].device_id;
+
+        let promise;
+        switch(args.instances) {
+            case 0:
+                promise = cluster.setPoePorts(device_id, [false, false, false, true]);
+            break;
+            case 1:
+                promise = cluster.setPoePorts(device_id, [true, false, false, true]);
+            break;
+            case 2:
+                promise = cluster.setPoePorts(device_id, [true, true, false, true]);
+            break;
+            case 3:
+                promise = cluster.setPoePorts(device_id, [true, true, true, true]);
+            break;
+            default:
+                throw new Error('this number of instances is not supported');
+            break;
+        }
+
+        return promise.then(response => {
+            return [];
+        });
     });
 };
