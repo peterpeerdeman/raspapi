44ba407d63c0824872b829a0765a12f65ecbca46
Peter Peerdeman <peter@lifely.nl>
2021-05-16 18:15:37 +0200

    fix(deps): turn pvoutput into public package

--- routes/solar/index.js
+++ routes/solar/index.js
@@ -1,30 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-var PVoutputAPI = require('pvoutput-nodejs');
+var PVoutputAPI = require('pvoutput');
 
 var pvoutput = new PVoutputAPI({
     debug: false,
     apiKey: process.env.PVOUTPUT_APIKEY,
-    systemId: process.env.PVOUTPUT_SYSTEMID
+    systemId: process.env.PVOUTPUT_SYSTEMID,
 });
 
-router.get('/status', function(req, res) {
-    return pvoutput.getStatus().then(function(results) {
-        res.send(results);
-    })
-    .catch(function(err) {
-        throw err;
-    });
+router.get('/status', function (req, res) {
+    return pvoutput
+        .getStatus()
+        .then(function (results) {
+            res.send(results);
+        })
+        .catch(function (err) {
+            throw err;
+        });
 });
 
-router.get('/output', function(req, res) {
-    return pvoutput.getOutput().then(function(results) {
-        res.send(results);
-    })
-    .catch(function(err) {
-        throw err;
-    });
+router.get('/output', function (req, res) {
+    return pvoutput
+        .getOutput()
+        .then(function (results) {
+            res.send(results);
+        })
+        .catch(function (err) {
+            throw err;
+        });
 });
 
 module.exports = router;
