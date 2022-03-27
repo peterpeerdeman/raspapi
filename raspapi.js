30e7681746f2d6142eaf18a84b897d5081acb5f1
Peter Peerdeman <peterpeerdeman@gmail.com>
2022-03-27 17:48:33 +0200

    fix(graphql): fix graphql apollo3 migration and authentication

--- raspapi.js
+++ raspapi.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const http = require('http');
 const fs = require('fs');
 const auth = require('basic-auth');
 const bodyParser = require('body-parser');
@@ -12,6 +12,7 @@ const cluster = require('./resolvers/cluster.js');
 const music = require('./resolvers/music.js');
 
 const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 
 // const Slimbot = require('slimbot');
 // const slimbotEndpoints = require('./slimbot-endpoints.js');
@@ -123,137 +124,93 @@ const resolvers = {
     },
 };
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-        return {
-            myProperty: true,
-        };
-    },
-    playground: {
-        settings: {
-            'editor.theme': 'light',
+async function startApolloServer(typeDefs, resolvers) {
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: async ({ req }) => {
+            const token = req.headers.authorization || '';
+            const correctAuth = `Basic ${new Buffer.from(
+                process.env.API_USER + ':' + process.env.API_PASS,
+                'utf-8'
+            ).toString('base64')}`;
+
+            if (token !== correctAuth)
+                throw new AuthenticationError('you must be logged in');
+            return {
+                user: process.env.API_USER,
+            };
         },
-        tabs: [
-            {
-                endpoint: process.env.API_GRAPHQL,
-                query: 'query {\n  hello\n}',
-                name: 'Raspapi',
-                headers: {
-                    Authorization: `Basic ${new Buffer.from(
-                        process.env.API_USER + ':' + process.env.API_PASS,
-                        'utf-8'
-                    ).toString('base64')}`,
-                },
-            },
-            {
-                endpoint: process.env.API_GRAPHQL,
-
-                query: gql`
-                    mutation {
-                        cluster_scale(instances: 2) {
-                            name
-                        }
-                    }
-                `,
-                name: 'Raspapi cluster mutation unifi',
-                headers: {
-                    Authorization: `Basic ${new Buffer.from(
-                        process.env.API_USER + ':' + process.env.API_PASS,
-                        'utf-8'
-                    ).toString('base64')}`,
-                },
-            },
-            {
-                endpoint: process.env.API_GRAPHQL,
-                query: gql`
-                    query {
-                        cluster {
-                            portTable {
-                                poe_power
-                                poe_enable
-                            }
-                        }
-                    }
-                `,
-                name: 'Raspapi cluster query',
-                headers: {
-                    Authorization: `Basic ${new Buffer.from(
-                        process.env.API_USER + ':' + process.env.API_PASS,
-                        'utf-8'
-                    ).toString('base64')}`,
-                },
-            },
-        ],
-    },
-});
-
-//parsing
-app.use(
-    bodyParser.json({
-        type: '*/*',
-    })
-);
-
-//authorization
-app.use(function (req, res, next) {
-    var credentials = auth(req);
-    if (
-        !credentials ||
-        credentials.name !== process.env.API_USER ||
-        credentials.pass !== process.env.API_PASS
-    ) {
-        res.set({
-            'WWW-Authenticate': 'Basic realm="simple-admin"',
-        });
-        res.sendStatus(401);
-    } else {
-        next();
-    }
-});
-
-//pretty jsonresponses
-app.use(function (req, res, next) {
-    res.sendStatusJson = function (status) {
-        res.status(status).send({ status: status });
-    };
-    next();
-});
-
-//routes
-routes = fs.readdirSync('./routes');
-routes.forEach(function (routeFile) {
-    app.use('/api/' + routeFile, require('./routes/' + routeFile));
-});
-
-//graphql
-server.applyMiddleware({ app });
-
-//errorhandling
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send({
-        error: '500',
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
-    next(err);
-});
 
-// const slimbot = new Slimbot(process.env['TELEGRAM_BOT_TOKEN']);
-// slimbotEndpoints.init(slimbot);
-// slimbot.startPolling();
+    //parsing
+    app.use(
+        bodyParser.json({
+            type: '*/*',
+        })
+    );
 
-// hide h eaders
-app.disable('x-powered-by');
-app.listen(
-    {
-        port: process.env.RASPAPI_PORT,
-    },
-    () => {
-        console.log(
-            `${new Date().toISOString()} raspapi listening at ${
-                process.env.RASPAPI_PORT
-            }`
-        );
-    }
-);
+    //authorization /api routes
+    app.use('/api', function (req, res, next) {
+        var credentials = auth(req);
+        if (
+            !credentials ||
+            credentials.name !== process.env.API_USER ||
+            credentials.pass !== process.env.API_PASS
+        ) {
+            res.set({
+                'WWW-Authenticate': 'Basic realm="simple-admin"',
+            });
+            res.sendStatus(401);
+        } else {
+            next();
+        }
+    });
+
+    //pretty jsonresponses
+    app.use(function (req, res, next) {
+        res.sendStatusJson = function (status) {
+            res.status(status).send({ status: status });
+        };
+        next();
+    });
+
+    //routes
+    routes = fs.readdirSync('./routes');
+    routes.forEach(function (routeFile) {
+        app.use('/api/' + routeFile, require('./routes/' + routeFile));
+    });
+
+    //errorhandling
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send({
+            error: '500',
+        });
+        next(err);
+    });
+
+    // const slimbot = new Slimbot(process.env['TELEGRAM_BOT_TOKEN']);
+    // slimbotEndpoints.init(slimbot);
+    // slimbot.startPolling();
+
+    // hide h eaders
+    app.disable('x-powered-by');
+
+    await server.start();
+    server.applyMiddleware({ app, path: '/graphql' });
+
+    await new Promise((resolve) =>
+        httpServer.listen({ port: process.env.RASPAPI_PORT }, resolve)
+    );
+    console.log(
+        `${new Date().toISOString()} raspapi listening at ${
+            process.env.RASPAPI_PORT
+        }`
+    );
+}
+
+startApolloServer(typeDefs, resolvers);
